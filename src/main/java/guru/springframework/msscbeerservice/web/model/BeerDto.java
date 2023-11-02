e9e6856acef2eb28a45ccfa35529255cc9a36ce9
Santiago Montano <slincem@gmail.com>
2023-11-01 21:59:08 -0500

    Adding validations to DTO

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,6 +1,10 @@
 package guru.springframework.msscbeerservice.web.model;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,16 +21,30 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyleEnum;
 
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOfHand;
